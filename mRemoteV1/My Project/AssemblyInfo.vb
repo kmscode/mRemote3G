94791e7d43ac478e95541012a720422f619d1c36
Kaim, Sean M <Sean.Kaim@ca.com>
2015-11-12 14:33:23 -0500

    Version 1.82
    Fixed a number of Complier Warnings
    Print exception stack in the log
    PuTTYNG updated to 0.66 from Upstream PuTTY release

--- mRemoteV1/My Project/AssemblyInfo.vb
+++ mRemoteV1/My Project/AssemblyInfo.vb
@@ -32,6 +32,6 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.81.*")>
+<Assembly: AssemblyVersion("1.82.*")>
 
 <Assembly: NeutralResourcesLanguageAttribute("en")> 
