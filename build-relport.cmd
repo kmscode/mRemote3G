43ab0e40cb696132f1c8b24a8d9c8aed70652db8
Kaim, Sean M <Sean.Kaim@ca.com>
2015-11-16 22:26:36 -0500

    new build-relport.cmd file
    Added new build-relport.cmd file for quick "Release Portable" builds

---
+++ build-relport.cmd
@@ -0,0 +1,2 @@
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\VsDevCmd.bat"
+"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" "D:\Source\mRemoteNG\mRemoteV1.sln" /Rebuild "Release Portable"
