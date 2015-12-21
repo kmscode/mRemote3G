e6cff979fbb8be0b7874409758f65a4212e623ec
Kaim, Sean M <Sean.Kaim@ca.com>
2015-12-20 22:59:59 -0500

    Update build batch file
    Remove undesired files if they exist

--- build-relport.cmd
+++ build-relport.cmd
@@ -4,6 +4,13 @@ call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\VsDevCmd
 rmdir /S /Q D:\Source\mRemoteNG\mRemoteV1\bin\package
 mkdir D:\Source\mRemoteNG\mRemoteV1\bin\package
 copy D:\Source\mRemoteNG\*.txt D:\Source\mRemoteNG\mRemoteV1\bin\package
+
+rem These del's can error out, that's OK. We don't want these files in the release.
+del "D:\Source\mRemoteNG\mRemoteV1\bin\Release Portable\confCons*"
+del "D:\Source\mRemoteNG\mRemoteV1\bin\Release Portable\mRemoteNG.log"
+del "D:\Source\mRemoteNG\mRemoteV1\bin\Release Portable\pnlLayout.xml"
+del "D:\Source\mRemoteNG\mRemoteV1\bin\Release Portable\extApps.xml"
+
 xcopy /S /Y "D:\Source\mRemoteNG\mRemoteV1\bin\Release Portable" D:\Source\mRemoteNG\mRemoteV1\bin\package
 
 for /f "delims=" %%i in ('findstr /R /C:"^<Assembly: AssemblyVersion" "mRemoteV1\My Project\AssemblyInfo.vb"') do set output="%%i"
